e94e117245c88771c5f7898810e8566d889c04dc
giwonjung <2c6829@naver.com>
2020-04-20 23:28:09 +0900

    mvc class 1

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-import express from "express";
+import express, { Router } from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-
-const handleProfile = (request, response) =>
-  response.send("you requested me a /profile methode");
-const handleHome = (request, response) => response.send("HI Chrome Brownser!");
-
-const middleWare = (req, res, next) => {
-  res.send("disconnectiong from middleware");
-};
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -20,15 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-app.use(middleWare);
-app.get("/profile", handleProfile);
-app.get("/cal/randomadd", (request, response) => {
-  const num1 = Math.floor(Math.random() * 20) + 1;
-  const num2 = Math.floor(Math.random() * 10) + 1;
-  const result = num1 + num2;
-
-  response.send(`num1(${num1})+num2(${num2})=${result} 입니다.`);
-});
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
